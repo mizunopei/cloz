c70ced18b75e6cdafb627fd1c1af59b55587271d
mizunopei <m.shuhei0309@gmail.com>
2019-11-25 18:48:44 +0900

    アクション修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,14 +1,25 @@
 class ItemsController < ApplicationController
   
   def index
+    @items = Item.all
   end
 
   def new
+    @item = Item.new
   end
 
   def create
+    Item.create(items_params)
+    redirect_to action: "index"
   end
 
   def destory
   end
+
+
+private
+  def items_params
+    params.require(:item).permit(:name, :image)
+  end
+
 end
