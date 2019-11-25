736c31c694d782fb8e6ee19c32d818205749f396
mizunopei <m.shuhei0309@gmail.com>
2019-11-25 18:46:41 +0900

    itemモデル修正

--- db/migrate/20191022084015_create_items.rb
+++ db/migrate/20191022084015_create_items.rb
@@ -1,10 +1,8 @@
 class CreateItems < ActiveRecord::Migration[5.2]
   def change
     create_table :items do |t|
-      t.text         :name   null:false
-      t.text         :image
-      t.references   :category, foreign_key:true
-      t.references   :color, foreign_key:true
+      t.string         :name, null: false
+      t.string         :image
       t.timestamps
     end
   end
