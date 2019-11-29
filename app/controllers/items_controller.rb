9ea1f3df683d25969775f3b7aa4af72893ce28b6
mizunopei <m.shuhei0309@gmail.com>
2019-11-29 19:10:39 +0900

    ルーティングの追加に伴うコントローラー編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -20,6 +20,53 @@ class ItemsController < ApplicationController
   def destory
   end
 
+  def outer
+    @outer__items = Item.where(category_id: "1", user_id: current_user.id)
+  end
+
+  def tops
+    @tops__items = Item.where(category_id: "2", user_id: current_user.id)
+  end
+
+  def bottoms
+    @bottoms__items = Item.where(category_id: "3", user_id: current_user.id)
+  end
+
+  def shoes
+    @shoes__items = Item.where(category_id: "4", user_id: current_user.id)
+  end
+
+  def goods
+    @goods__items = Item.where(category_id: "5", user_id: current_user.id)
+  end
+
+  def black
+    @black__items = Item.where(color_id: "1", user_id: current_user.id)
+  end
+
+  def white
+    @white__items = Item.where(color_id: "2",user_id: current_user.id)
+  end
+
+  def navy
+    @navy__items = Item.where(color_id: "3", user_id: current_user.id)
+  end
+
+  def gray
+    @gray__items = Item.where(color_id: "4", user_id: current_user.id)
+  end
+
+  def beige
+    @beige__items = Item.where(color_id: "5", user_id: current_user.id)
+  end
+
+  def green
+    @green__items = Item.where(color_id: "6", user_id: current_user.id)
+  end
+
+  def other_color
+    @other_color__items = Item.where(color_id: "7", user_id: current_user.id)
+  end
 
 private
   def items_params
