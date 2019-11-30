65ea0e3af86657e2d56a5724770f3eb8acddc9dc
mizunopei <m.shuhei0309@gmail.com>
2019-11-30 15:24:27 +0900

    callbackによる記述簡略化

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,8 @@
 class ItemsController < ApplicationController
+
+  before_action :display_items, execpt:[:new, :create, :destory, :edit]
   
   def index
-    @items = Item.where(user_id: current_user.id).page(params[:page]).per(8)
   end
 
   def new
@@ -21,56 +22,60 @@ class ItemsController < ApplicationController
   end
 
   def outer
-    @outer__items = Item.where(category_id: "1", user_id: current_user.id).page(params[:page]).per(8)
+    @outer__items = @items.where(category_id: "1")
   end
 
   def tops
-    @tops__items = Item.where(category_id: "2", user_id: current_user.id).page(params[:page]).per(8)
+    @tops__items = @items.where(category_id: "2")
   end
 
   def bottoms
-    @bottoms__items = Item.where(category_id: "3", user_id: current_user.id).page(params[:page]).per(8)
+    @bottoms__items = @items.where(category_id: "3")
   end
 
   def shoes
-    @shoes__items = Item.where(category_id: "4", user_id: current_user.id).page(params[:page]).per(8)
+    @shoes__items = @items.where(category_id: "4")
   end
 
   def goods
-    @goods__items = Item.where(category_id: "5", user_id: current_user.id).page(params[:page]).per(8)
+    @goods__items = @items.where(category_id: "5")
   end
 
   def black
-    @black__items = Item.where(color_id: "1", user_id: current_user.id).page(params[:page]).per(8)
+    @black__items = @items.where(color_id: "1")
   end
 
   def white
-    @white__items = Item.where(color_id: "2",user_id: current_user.id).page(params[:page]).per(8)
+    @white__items = @items.where(color_id: "2")
   end
 
   def navy
-    @navy__items = Item.where(color_id: "3", user_id: current_user.id).page(params[:page]).per(8)
+    @navy__items = @items.where(color_id: "3")
   end
 
   def gray
-    @gray__items = Item.where(color_id: "4", user_id: current_user.id).page(params[:page]).per(8)
+    @gray__items = @items.where(color_id: "4")
   end
 
   def beige
-    @beige__items = Item.where(color_id: "5", user_id: current_user.id).page(params[:page]).per(8)
+    @beige__items = @items.where(color_id: "5")
   end
 
   def green
-    @green__items = Item.where(color_id: "6", user_id: current_user.id).page(params[:page]).per(8)
+    @green__items = @items.where(color_id: "6")
   end
 
   def other_color
-    @other_color__items = Item.where(color_id: "7", user_id: current_user.id).page(params[:page]).per(8)
+    @other_color__items = @items.where(color_id: "7")
   end
 
 private
   def items_params
     params.require(:item).permit(:name, :image, :category_id, :color_id, :price).merge(user_id: current_user.id)
+  end
+
+  def display_items
+    @items = Item.where(user_id: current_user.id).page(params[:page]).per(8)
   end
 
 end
