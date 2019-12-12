363a32eab9bb29e39ca0cfbe8368c79a077a9520
mizunopei <m.shuhei0309@gmail.com>
2019-12-12 16:36:33 +0900

    colorモデル修正

--- app/models/color.rb
+++ app/models/color.rb
@@ -1,6 +1,6 @@
 class Color < ActiveHash::Base
   self.data = [
     {id: 1, name: "Black"},{id: 2, name: "White"},{id: 3, name: "Navy"},
-    {id: 4, name: "Gray"},{id:5, name: "beige"} ,{id:6, name: "Green"}, {id: 7, name: "Other"}
+    {id: 4, name: "Gray"},{id:5, name: "Beige"} ,{id:6, name: "Green"}, {id: 7, name: "Other"}
   ]
 end
