54b2406335da9570d97c8ff75bdb37d7fada4f62
mizunopei <m.shuhei0309@gmail.com>
2019-12-12 16:36:58 +0900

    itemモデルアクションの定義

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -1,2 +1,11 @@
 module ItemsHelper
+  def compare_price(last_month, this_month)
+    if last_month == this_month
+      content_tag :div, "(前月比 ±０)", class: "mom"
+    elsif last_month > this_month
+      content_tag :div, "(前月比 - " + (last_month - this_month).to_s(:delimited) +")", class: "mom decrease"
+    else
+      content_tag :div, "(前月比 +" + (last_month - this_month).abs.to_s(:delimited) + ")", class: "mom increase"
+    end
+  end
 end
