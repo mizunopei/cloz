46816b11769bb58ed8e7df7a5faca08a36fdf862
mizunopei <m.shuhei0309@gmail.com>
2019-12-17 18:15:50 +0900

    left-bar 修正

--- app/assets/javascripts/left-bar.js
+++ app/assets/javascripts/left-bar.js
@@ -4,9 +4,13 @@ $(function(){
 		$("li", nav)
 		.mouseover(function(e){
 			$("ul",this).stop().slideDown("fast");
+			var select = $(this).children(".select")[0];
+			$(select).toggleClass("select_a");
 		})
 		.mouseout(function(e){
 			$("ul", this).stop().slideUp("fast");
+			var select = $(this).children(".select")[0];
+			$(select).toggleClass("select_a");
 		});
 	})
 })
